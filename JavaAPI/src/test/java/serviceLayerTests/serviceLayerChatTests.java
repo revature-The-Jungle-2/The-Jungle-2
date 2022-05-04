f1a77f808a38bf96c3d6d0c94983011a294d1fe9
gracegegeorge <gracegeorge@hotmail.com>
2022-05-04 17:55:44 -0400

    service chat test updates

--- JavaAPI/src/test/java/serviceLayerTests/serviceLayerChatTests.java
+++ JavaAPI/src/test/java/serviceLayerTests/serviceLayerChatTests.java
@@ -1,10 +1,40 @@
 package serviceLayerTests;
-
+import dev.com.thejungle.customexception.InvalidInputException;
 import dev.com.thejungle.dao.implementations.ChatDAO;
+import dev.com.thejungle.entity.ChatMessage;
 import dev.com.thejungle.service.implementations.ChatService;
 import org.testng.annotations.Test;
 
 public class serviceLayerChatTests {
 
-    ChatService chatService = new ChatService(new ChatDAO();
+    ChatService chatService = new ChatService(new ChatDAO());
+
+
+    @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid User ID")
+    public void serviceCreateMessageFailInvalidUserId(){
+        ChatMessage chatMessage = new ChatMessage(0, 20, "Jungle");
+        chatService.serviceCreateMessageObject(chatMessage);
+    }
+
+    //@Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid Chat Content")
+    //public void serviceCreateMessageFailInvalidChatContent(){
+      //  ChatMessage chatMessage = new ChatMessage(1,1," ");
+       // chatService.serviceCreateMessageObject(chatMessage);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
 

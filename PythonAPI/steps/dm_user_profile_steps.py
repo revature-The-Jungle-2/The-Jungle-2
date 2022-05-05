74d8b8df266ebe0b84703c17d91cf9f75afe739a
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2022-05-05 14:39:19 -0500

    steps need to be implemented

---
+++ PythonAPI/steps/dm_user_profile_steps.py
@@ -0,0 +1,84 @@
+from behave import given, when, then
+
+@given(u'I am on the login page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given I am on the login page')
+
+
+@when(u'I enter my <username>')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter my username')
+
+
+@when(u'I enter my <password>')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter my password')
+
+
+@when(u'I click the login button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the login button')
+
+
+@when(u'I am on the profile page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I am on the profile page')
+
+
+@when(u'I click the profile picture')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the profile picture')
+
+
+@when(u'I choose my downloads/Husky.jpg')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I choose my downloads/Husky.jpg')
+
+
+@then(u'I am back on the profile page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I am back on the profile page')
+
+
+@when(u'I enter my <about_me> incorrectly')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter my <about_me> incorrectly')
+
+
+@when(u'I enter my <birthday> incorrectly')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter my <birthday> incorrectly')
+
+
+@when(u'I enter my <about_me>')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter my <about_me>')
+
+
+@when(u'I enter my <birthday>')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter my <birthday>')
+
+
+@when(u'I click the save changes button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the save changes button')
+
+
+@then(u'I am left in the pop up')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I am left in the pop up')
+
+@given(u'I am on the profile page')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given I am on the profile page')
+
+
+@when(u'I click on my follower')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click on my follower')
+
+
+@then(u'I am on their profile')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I am on their profile')

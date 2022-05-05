201c8ffba2c6861ba2dee015c2858b2f02b5e67a
jraghuraj <jraghuraj@ufl.edu>
2022-05-05 15:13:43 -0400

    updated unit chat unit tests

--- JavaAPI/src/test/java/serviceLayerTests/serviceLayerChatTests.java
+++ JavaAPI/src/test/java/serviceLayerTests/serviceLayerChatTests.java
@@ -7,27 +7,35 @@ import dev.com.thejungle.entity.ChatMessage;
 import dev.com.thejungle.service.implementations.ChatService;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 public class serviceLayerChatTests {
     ChatService chatservice = new ChatService(new ChatDAO());
 
 
     @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid Group ID")
     public void serviceCreateMessageObjectInvalidGroupId() {
-        ChatMessage chatMessage = new ChatMessage(1, 1, "message");
+        ChatMessage chatMessage = new ChatMessage(1, -1, "message");
         chatservice.serviceCreateMessageObject(chatMessage);
 
     }
 
-    @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid Chat Content")
+    @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Long Content")
     public void serviceCreateMessageObjectInvalidChatContent() {
-        ChatMessage chatMessage = new ChatMessage(1, 2, "message");
+        ChatMessage chatMessage = new ChatMessage(1, 1, "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
         chatservice.serviceCreateMessageObject(chatMessage);  
     }
 
     @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid User ID")
     public void serviceCreateMessageFailInvalidUserId(){
         ChatMessage chatMessage = new ChatMessage(0, 20, "Jungle");
-        chatService.serviceCreateMessageObject(chatMessage);
+        chatservice.serviceCreateMessageObject(chatMessage);
+    }
+
+    @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid Input Exception")
+    public void serviceGetMessageHistoryNoGroupID(){
+//        ArrayList<ChatMessage> testGetMessage = daoChatObject.getMessageHistory(-1);
+        chatservice.serviceGetMessageHistory(0);
     }
 }
   

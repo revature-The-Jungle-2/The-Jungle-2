4ff98d96e5928c72789a03fa83aa03c015ff7bbf
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2022-05-05 09:08:13 -0500

    started e2e stuff

--- PythonAPI/environment.py
+++ PythonAPI/environment.py
@@ -1,11 +1,15 @@
 from behave.runner import Context
 from selenium import webdriver
+from selenium.webdriver.safari.webdriver import WebDriver
+
+from poms.dm_user_profile_pom import UserProfile
+
 
 def before_all(context: Context):
-    context.driver = webdriver.Chrome("chromedriver.exe") #change this as needed
-
+    #context.driver = webdriver.Chrome("chromedriver.exe") #change this as needed
+    context.driver = WebDriver()
     ###PUT YOUR POM CONTEXTS BETWEEN THESE LINES.###
-
+    context.UserProfile = UserProfile(context.driver)
 
 
     ###PUT YOUR POM CONTEXTS BETWEEN THESE LINES.###

33fa141ccd30260d5f890f3220b2eac8c8a812be
Samapuma <79788895+SamanthaWathenOlson@users.noreply.github.com>
2022-05-05 10:22:04 -0400

    Testing for Service layer

--- JavaAPI/src/test/java/serviceLayerTests/serviceLayerChatTests.java
+++ JavaAPI/src/test/java/serviceLayerTests/serviceLayerChatTests.java
@@ -1,31 +1,26 @@
 package serviceLayerTests;
 
 import dev.com.thejungle.customexception.InvalidInputException;
+import dev.com.thejungle.dao.implementations.ChatDAO;
 import dev.com.thejungle.entity.ChatMessage;
 import dev.com.thejungle.service.implementations.ChatService;
-import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class serviceLayerChatTests {
-
-    @Test()
-    public void createChatSuccess()
+    ChatService chatservice = new ChatService(new ChatDAO());
 
 
-    @Test(expectedExceptions = InvalidInputException, expectedExceptionsMessageRegExp = "Invalid User ID")
-    public void serviceCreateMessageObjectInvalidUserId(){
-        ChatMessage chatMessage = new ChatMessage();
-        ChatMessage result = ChatService.serviceCreateMessageObject(chatMessage);
-        Assert.assertNotSame(result.getUserId(int));
-    }
-
-    @Test(expectedExceptions = InvalidInputException, expectedExceptionsMessageRegExp = "Invalid Group ID")
-    public void serviceCreateMessageObjectInvalidGroupId(){
-        ChatMessage chatMessage = new ChatMessage();
-        ChatMessage result = ChatService.serviceCreateMessageObject(chatMessage);
-        Assert.assertNotSame(result.getGroupId(int));
-
+    @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid Group ID")
+    public void serviceCreateMessageObjectInvalidGroupId() {
+        ChatMessage chatMessage = new ChatMessage(1, 1, "message");
+        chatservice.serviceCreateMessageObject(chatMessage);
 
     }
-    )
+
+    @Test(expectedExceptions = InvalidInputException.class, expectedExceptionsMessageRegExp = "Invalid Chat Content")
+    public void serviceCreateMessageObjectInvalidChatContent() {
+        ChatMessage chatMessage = new ChatMessage(1, 2, "message");
+        chatservice.serviceCreateMessageObject(chatMessage);
+
+    }
 }

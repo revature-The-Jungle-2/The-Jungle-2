49874842675ccd5d277c80a26e56a7b05c9e2b04
William Blair <blaircorporate@gmail.com>
2022-05-05 10:26:25 -0500

    Added chromerdriver

--- PythonAPI/environment.py
+++ PythonAPI/environment.py
@@ -1,16 +1,16 @@
 from behave.runner import Context
 from selenium import webdriver
 
+
 def before_all(context: Context):
-    context.driver = webdriver.Chrome("chromedriver.exe") #change this as needed
+    context.driver = webdriver.Chrome("chromedriver.exe")  # change this as needed
 
     ###PUT YOUR POM CONTEXTS BETWEEN THESE LINES.###
-
-
 
     ###PUT YOUR POM CONTEXTS BETWEEN THESE LINES.###
 
     context.driver.implicitly_wait(1)
+
 
 def after_all(context: Context):
     context.driver.quit()

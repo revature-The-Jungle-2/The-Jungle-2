939102fce1ee385a73acd1e01d1efafb1d33fa02
Samapuma <79788895+SamanthaWathenOlson@users.noreply.github.com>
2022-05-06 18:24:38 -0400

    POMs for ChatPage

--- JavaAPI/src/test/java/endtoendTests/poms/ChatPage.java
+++ JavaAPI/src/test/java/endtoendTests/poms/ChatPage.java
@@ -7,12 +7,26 @@ import org.openqa.selenium.support.PageFactory;
 public class ChatPage {
     WebDriver driver;
 
+
     private By searchBox = By.id("search-container");
     private By writeMessage = By.xpath("/html/body/div/div/div[2]/div/div[1]/div[3]/div/div/input");
+    private By chatLink = By.xpath("/html/body/div/div/div[1]/div[2]/div[1]/a/span");
+    private By groupLink = By.xpath("/html/body/div/div/div[1]/div[2]/div[2]/a/span")
 
     public ChatPage(WebDriver driver) {
         this.driver = driver;
 
         PageFactory.initElements(driver,this);
     }
+
+    public void clickChatLink() { driver.findElement(chatLink).click(); }
+
+    public void sendKeysToChatLink(String input) { driver.findElement(chatLink).sendKeys(input); }
+
+    public void clickWriteMessage(String input) { driver.findElement(writeMessage).click(); }
+
+    public void sendKeysToWriteMessage(String input) { driver.findElement(writeMessage).click(); }
+
+    
+
 }

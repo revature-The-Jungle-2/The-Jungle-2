7b53a8c5f666db5e725633dc37d5e226940f2f9b
Tashawn <tashawnhuggins@gmail.com>
2022-05-09 12:41:36 -0400

    pushing changes

--- JavaAPI/src/test/java/endtoendTests/steps/UnregisteredUserSignUp.java
+++ JavaAPI/src/test/java/endtoendTests/steps/UnregisteredUserSignUp.java
@@ -1,23 +1,49 @@
 package endtoendTests.steps;
 
+import endtoendTests.runner.TestRunner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+import static endtoendTests.runner.TestRunner.driver;
 
 public class UnregisteredUserSignUp {
+    int min = 1;
+    int max = 10000;
+    int b = (int)(Math.random()*(max-min+1)+min);
+    int c = (int)(Math.random()*(max-min+1)+min);
+    Actions act = new Actions(driver);
+    String url = driver.getCurrentUrl();
+
     @Given("i am on the login page")
-    public void i_am_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+//    public void i_am_on_the_login_page() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
     @When("i click on the sign up link")
     public void i_click_on_the_sign_up_link() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.signupPage.clickSignupFirstName();
+        TestRunner.signupPage.sendKeysToFirstNameSignup("Jane");
+        TestRunner.signupPage.clickSignupLastName();
+        TestRunner.signupPage.sendKeysToLastNameSignup("Doe");
+        TestRunner.signupPage.clickSignupEmail();
+        TestRunner.signupPage.sendKeysSignupEmail("JaneDoe@gmail.com");
+        act.sendKeys(Keys.TAB).build().perform();
+        TestRunner.signupPage.sendKeysSignupBirthdate("19901121");
+        TestRunner.signupPage.clickSignupUsername();
+        TestRunner.signupPage.sendKeysSignupUsername("JaneDoe");
+        TestRunner.signupPage.clickSignupPassword();
+        TestRunner.signupPage.sendKeysSignupPassword("janedoe1");
     }
     @Then("i am redirected to the registration page")
     public void i_am_redirected_to_the_registration_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        String title = driver.getCurrentUrl();
+        Assert.assertEquals("http://the-jungle-2-rev-bucket.s3-website-us-east-1.amazonaws.com/FrontEnd/registrationpage/sign-up.html", title);
     }
 }

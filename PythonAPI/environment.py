37dd9af7dc3743af722c755e1c904cced71d3fc6
yeonghwanchoi <yeonghwanchoi1@gmail.com>
2022-05-09 10:12:46 -0400

    update

--- PythonAPI/environment.py
+++ PythonAPI/environment.py
@@ -4,17 +4,16 @@ from selenium.webdriver.safari.webdriver import WebDriver
 
  
 from poms.dm_user_profile_pom import UserProfile
+from poms.group_page import GroupPage
+from poms.individual_group_page import individualGroupPage
 
 
 def before_all(context: Context):
-    #context.driver = webdriver.Chrome("chromedriver.exe") #change this as needed
-    context.driver = WebDriver()
-    ###PUT YOUR POM CONTEXTS BETWEEN THESE LINES.###
+    context.driver = webdriver.Chrome("PythonAPI/features/chromedriver.exe")
+    context.groupPage = GroupPage(context.driver)
+    context.individualGroupPage = individualGroupPage(context.driver)
     context.UserProfile = UserProfile(context.driver)
 
-
-
-    ###PUT YOUR POM CONTEXTS BETWEEN THESE LINES.###
 
     context.driver.implicitly_wait(1)
 

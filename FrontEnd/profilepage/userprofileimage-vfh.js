4f78e2a23506e9bdf1b691f9219c0a03302d7fb5
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2022-05-09 08:43:08 -0700

    reverting front end changes

--- FrontEnd/profilepage/userprofileimage-vfh.js
+++ FrontEnd/profilepage/userprofileimage-vfh.js
@@ -1,6 +1,6 @@
 const devUrlJava="http://44.200.50.0:8000";
-//const devUrlPython="http://ec2-204-236-138-16.us-west-1.compute.amazonaws.com:5000"
-const devUrlPython="http://127.0.0.1:5000"
+const devUrlPython="http://ec2-204-236-138-16.us-west-1.compute.amazonaws.com:5000"
+
 // let userId = 9000;
 
 async function getUserImage(){

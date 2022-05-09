89539296f667afc8f32709e76a405bd3bd1e081d
Samapuma <79788895+SamanthaWathenOlson@users.noreply.github.com>
2022-05-09 08:45:15 -0400

    POMs for ChatPage

--- JavaAPI/src/test/java/endtoendTests/poms/ChatPage.java
+++ JavaAPI/src/test/java/endtoendTests/poms/ChatPage.java
@@ -11,7 +11,8 @@ public class ChatPage {
     private By searchBox = By.id("search-container");
     private By writeMessage = By.xpath("/html/body/div/div/div[2]/div/div[1]/div[3]/div/div/input");
     private By chatLink = By.xpath("/html/body/div/div/div[1]/div[2]/div[1]/a/span");
-    private By groupLink = By.xpath("/html/body/div/div/div[1]/div[2]/div[2]/a/span")
+    private By groupLink = By.xpath("/html/body/div/div/div[1]/div[2]/div[2]/a/span");
+    private By messageButton = By.xpath("")
 
     public ChatPage(WebDriver driver) {
         this.driver = driver;
@@ -27,6 +28,12 @@ public class ChatPage {
 
     public void sendKeysToWriteMessage(String input) { driver.findElement(writeMessage).click(); }
 
-    
+    public void sendKeysToWriteMessage(String input){ driver.findElement(writeMessage).sendKeys(input); }
+
+    public void clickMessageButton() { driver.findElement(messagedButton).click(); }
+
+    public void sendKeysToMessageButton(String input) { driver.findElement(messageButton).sendKeys(input); }
+
+
 
 }
